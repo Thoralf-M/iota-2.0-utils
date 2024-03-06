d60d217926a51750bf8591c5b23809c2150de75d
Thoralf Müller <thoralf.mue@gmail.com>
2024-03-06 11:34:39 +0100

    Add nativeToken query parameter

--- src/lib/pages/Indexer.svelte
+++ src/lib/pages/Indexer.svelte
@@ -22,6 +22,7 @@
         unlockableByAddress: "",
         address: "",
         tag: "",
+        nativeToken: "",
         timelockedBefore: "",
         timelockedAfter: "",
         createdBefore: "",
